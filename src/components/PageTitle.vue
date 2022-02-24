5f0510d9fa9032c1cfe27b8171c32217814736e7
2022project <ghkdsigm3@gmail.com>
2022-02-24 17:13:02 +0900

    Create project

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>{{title}}</h2>
+  <h3>{{likes}}</h3>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "페이지 제목입니다."
+    },
+    likes:{
+      type: Number,
+      
+    }
+  }
+}
+</script>
---
+++ src/views/ParentComponent4.vue
@@ -0,0 +1,22 @@
+<template>
+ <child-component @send-message="sendMessage" />
+ <div>
+   {{newData}}
+ </div>
+</template>
+<script>
+import ChildComponent from './ChildComponent4';
+export default {
+ components: {ChildComponent},
+ data(){
+   return{
+     newData:''
+   }
+ },
+ methods: {
+   sendMessage(data) {
+     this.newData = data
+   }
+ }
+}
+</script>
